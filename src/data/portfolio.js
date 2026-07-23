7fa072a4c2a1e92ba549d2b41b1c3eb5bde78e71
naufalnak <naufal.ndak17@gmail.com>
2026-07-23 09:22:26 +0700

    update : fix link resumeUrl

--- src/data/portfolio.js
+++ src/data/portfolio.js
@@ -13,7 +13,7 @@ export const personal = {
     "Specializing in RESTful API development with Node.js and Express.js, Android mobile development using Kotlin and Jetpack Compose, and UI/UX design. I care deeply about code quality, structured architecture, and building technology solutions that make a real impact.",
   availability: true,
   resumeUrl:
-    "https://drive.google.com/file/d/1-FO-in0keMau3afb8fMK9VV2r23PFR0J/view?usp=drive_link",
+    "https://drive.google.com/file/d/1gCmO1_F8REvMU2fFUQKAqgUvidMl1opS/view?usp=drive_link",
   yearStarted: 2021,
 };
 
